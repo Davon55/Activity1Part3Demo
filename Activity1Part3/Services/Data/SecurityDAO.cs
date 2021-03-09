b4ff311097af0f042a37f212ded29fb23ac64305
Davon55 <55595879+Davon55@users.noreply.github.com>
2021-03-09 03:01:34 -0700

    Update SecurityDAO.cs

--- Activity1Part3/Services/Data/SecurityDAO.cs
+++ Activity1Part3/Services/Data/SecurityDAO.cs
@@ -10,15 +10,15 @@ namespace Activity1Part3.Services.Data
     public class SecurityDAO
     {
         //connect to the database 
-        string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Test;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
 
-        public bool FinByUser(UserModel user)
+        public bool FindByUser(UserModel user)
         {
             //start by assuming that nothing is found in the query 
             bool success = false;
 
             // write sql expression 
-            string queryString = "SELECT * FROM dbo.Users WHERE USERNAME = @userName AND PASSWORD = @Password"; 
+            string queryString = "SELECT * FROM dbo.UsersTwo WHERE username = @Username AND password = @Password"; 
 
             //create and open the connetion to the database inside a using block
             //This ensures that all resources are closed properly when the query is done.
@@ -28,7 +28,7 @@ namespace Activity1Part3.Services.Data
                 SqlCommand command = new SqlCommand(queryString, connection);
 
                 // associate @username with the user.username
-                command.Parameters.Add("@userName", System.Data.SqlDbType.VarChar, 50).Value = user.userName;
+                command.Parameters.Add("@Username", System.Data.SqlDbType.VarChar, 50).Value = user.Username;
                 command.Parameters.Add("@Password", System.Data.SqlDbType.VarChar, 50).Value = user.Password;
 
                 //open the database and run the comman
